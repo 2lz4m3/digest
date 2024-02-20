9983dc4e3192dee7ac58e0f6bbc2fc1ef263ec67
2lz4m3 <157938150+2lz4m3@users.noreply.github.com>
2024-02-20 16:12:38 +0000

    Implement basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import FormGroup from '@mui/material/FormGroup';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import Switch from '@mui/material/Switch';
+import FormLabel from '@mui/material/FormLabel';
+import Container from '@mui/material/Container';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+
+type Algorithm = 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [multiline, setMultiline] = useState(false);
+  const [trim, setTrim] = useState(false);
+  const [algorithm, setAlgorithm] = useState<Algorithm>('SHA-1');
+
+  async function digestMessage(message: string): Promise<string> {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hash = await crypto.subtle.digest(algorithm.toString(), data);
+    const hashArray = Array.from(new Uint8Array(hash));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
+  }
+
+  async function updateOutput() {
+    if (multiline) {
+      const lines = input.split(/\r?\n/);
+      const promises = lines.map((line) => {
+        let message = line;
+        if (trim) {
+          message = line.trim();
+        }
+        return digestMessage(message);
+      });
+      await Promise.all(promises).then((values) => { setOutput(values.join('\n')) });
+    } else {
+      let message = input;
+      if (trim) {
+        message = input.trim();
+      }
+      await digestMessage(message).then((value) => { setOutput(value) });
+    }
+  }
+
+  useEffect(() => {
+    void updateOutput();
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth="sm">
+        <FormGroup sx={{ m: 2 }}>
+          <FormLabel component="legend">Input options</FormLabel>
+          <FormControlLabel
+            id="multiline"
+            control={<Switch />}
+            label="MultiLine"
+            checked={multiline}
+            onChange={() => {
+              setMultiline(prevState => !prevState);
+              void updateOutput();
+            }}
+          />
+          <FormControlLabel
+            id="trim"
+            control={<Switch />}
+            label="Trim"
+            checked={trim}
+            onChange={() => {
+              setTrim(prevState => !prevState);
+              void updateOutput();
+            }}
+          />
+          <FormControl>
+            <FormLabel id="algorithm">Algorithm</FormLabel>
+            <RadioGroup
+              row
+              value={algorithm}
+              onChange={(event) => {
+                setAlgorithm(event.target.value as Algorithm);
+              }}
+            >
+              <FormControlLabel value="SHA-1" control={<Radio />} label="SHA-1" />
+              <FormControlLabel value="SHA-256" control={<Radio />} label="SHA-256" />
+              <FormControlLabel value="SHA-384" control={<Radio />} label="SHA-384" />
+              <FormControlLabel value="SHA-512" control={<Radio />} label="SHA-512" />
+            </RadioGroup>
+          </FormControl>
+        </FormGroup>
+        <FormGroup sx={{ m: 2 }}>
+          <TextareaAutosize
+            id="input"
+            minRows={4}
+            maxRows={8}
+            placeholder="Input"
+            defaultValue=""
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+          />
+        </FormGroup>
+        <FormGroup sx={{ m: 2 }}>
+          <TextareaAutosize
+            id="output"
+            minRows={4}
+            maxRows={8}
+            placeholder="Output"
+            value={output}
+            readOnly
+          />
+        </FormGroup>
+      </Container>
     </>
   )
 }
